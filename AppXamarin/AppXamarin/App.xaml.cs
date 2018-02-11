0af9dd4a73a2fabaf465a31d50cb34b534f75d90
IIMullahmetov <IIMullahmetov@gmail.com>
2018-02-11 19:45:42 +0300

    Add project files.

---
+++ AppXamarin/AppXamarin/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using AppXamarin.Views;
+using Xamarin.Forms;
+
+namespace AppXamarin
+{
+	public partial class App : Application
+	{
+
+		public App ()
+		{
+			InitializeComponent();
+
+
+            MainPage = new MainPage();
+        }
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
